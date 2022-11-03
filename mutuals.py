f89b585fb6134435c8a971c91e571735813871f6
Daniel <danysilna@gmail.com>
2022-11-03 13:21:17 -0400

    Added the ability to check the intersection between more than two profiles.

--- mutuals.py
+++ mutuals.py
@@ -6,17 +6,24 @@ if __name__ == '__main__':
 
     api = tw.API(authKeys.liveHandler())
 
-    user1 = 'leighalexander'
-    user2 = 'sidefx'
+    users = ['leighalexander', 'BootsRiley', 'pitchfork']
 
-    set1 = followsIO.loadFollows(mode='usr', file=user1)
+    for x in range(len(users) - 1):
+        if x == 0:
+            set1 = followsIO.loadFollows(mode='usr', file=users[x])
+            set2 = followsIO.loadFollows(mode='usr', file=users[x+1])
 
-    set2 = followsIO.loadFollows(mode='usr', file=user2)
+            matches = set1.intersection(set2)
+            headerString = f'{users[x]}: {len(set1)}//////{users[x+1]}: {len(set2)}//////'
+        else:
+            newSet = followsIO.loadFollows(mode='usr', file=users[x+1])
 
-    matches = set1.intersection(set2)
+            matches = matches.intersection(newSet)
+            headerString += f'{users[x+1]}: {len(newSet)}//////'
 
-    print(f'{user1}: {len(set1)}//////{user2}: {len(set2)}')
+    print(headerString)
     print('')
+    print('Matches: ' + str(len(matches)))
     for match in matches:
         try:
             userID = api.get_user(user_id=match)
